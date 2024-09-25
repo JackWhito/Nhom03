e596172f4b91ab4e0931f58b36c15063e636af62
JackWhito <namtrung.work@gmail.com>
2024-09-25 15:26:35 +0700

    Upload UI

--- BaiTap03/register-app/frontend/src/components/Home.js
+++ BaiTap03/register-app/frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import HomeHeader from './HomeHeader';
 import HomeMain from './HomeMain';
+import HomeFooter from './HomeFooter';
 const HelloWorld = () => {
     return (
         <div>
